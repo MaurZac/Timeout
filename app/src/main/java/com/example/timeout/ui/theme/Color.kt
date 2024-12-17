d9b05397002d5aeb6aa37547305b44092c53ee63
MaurZac <maur.mx@gmail.com>
2024-12-17 13:35:47 -0600

    adding Font and Colors... Starting

---
+++ app/src/main/java/com/example/timeout/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.timeout.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryDark = Color(color = 0XFF000000)
+val primaryWhite = Color(color = 0XFFFFFFFF)
+val gray = Color(color = 0XFF888888)
+val lightGray = Color(color = 0XFFCCCCCC)
---
+++ app/src/main/java/com/example/timeout/ui/theme/Theme.kt
@@ -0,0 +1,63 @@
+package com.example.timeout.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = primaryWhite,
+    secondary = gray,
+    tertiary = lightGray,
+    surface = primaryDark,
+    background = primaryDark
+
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = primaryWhite,
+    secondary = gray,
+    tertiary = lightGray,
+    surface = primaryDark,
+    background = primaryDark
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun TimeOutTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
