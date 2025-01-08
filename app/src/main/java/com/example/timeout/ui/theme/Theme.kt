c9f32d81ff77af424daa408b93b4a03bc95d776a
MaurZac <maur.mx@gmail.com>
2025-01-08 09:20:56 -0600

    setting fonts and screen sizes

--- app/src/main/java/com/example/timeout/ui/theme/Theme.kt
+++ app/src/main/java/com/example/timeout/ui/theme/Theme.kt
@@ -8,8 +8,14 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.isDebugInspectorInfoEnabled
 
 private val DarkColorScheme = darkColorScheme(
     primary = primaryWhite,
@@ -38,11 +44,23 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val LocalDimens = staticCompositionLocalOf { DefaultDimens }
+
+@Composable
+fun ProvideDimens(
+    dimens: Dimens,
+    content: @Composable () -> Unit
+){
+    val dimensionSet = remember { dimens }
+    CompositionLocalProvider(LocalDimens provides dimensionSet, content = content)
+}
+
 @Composable
 fun TimeOutTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+    windowSize: WindowWidthSizeClass = WindowWidthSizeClass.Compact,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -55,9 +73,23 @@ fun TimeOutTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    val dimension = if(windowSize > WindowWidthSizeClass.Compact)
+            TabletDimens
+        else
+            DefaultDimens
+
+    ProvideDimens(dimens = dimension) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+}
+
+object FocusTimerYTTheme{
+    val dimens: Dimens
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalDimens.current
 }
